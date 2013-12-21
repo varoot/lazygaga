7321691a340c81592898fc9bfacc949b556cd29b
Hong Yoon Kim <hongyoon@umich.edu>
2013-12-21 10:20:19 -0500

    Add algorithm testing sandbox area

---
+++ algorithm/binPacking.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+
+function binPacking(people, housing, transportation) {
+	console.log('hi')
+	return input;
+}
+
+fs.readFile('undergrad-retreat.json', function (err, data) {
+	if (err) { throw err; }
+	var data = JSON.parse(data);
+	binPacking(data);
+});
+
+
